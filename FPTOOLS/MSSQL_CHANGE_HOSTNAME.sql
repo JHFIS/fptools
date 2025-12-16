934a6ed55d7950e3c0338981280d851bfb1a715b
JHFIS <jach.lin@fis.com.tw>
2025-12-16 13:25:58 +0800

    Add files via upload

---
+++ FPTOOLS/MSSQL_CHANGE_HOSTNAME.sql
@@ -0,0 +1,15 @@
+﻿IF SERVERPROPERTY('ServerName')<>@@SERVERNAME
+​
+BEGIN
+​
+  DECLARE @srvname sysname
+​
+  SET @srvname=@@SERVERNAME
+​
+  EXEC sp_dropserver @server=@srvname
+​
+  SET @srvname=CAST(SERVERPROPERTY('ServerName') as sysname)
+​
+  EXEC sp_addserver @server = @srvname , @local = 'LOCAL'
+​
+END
